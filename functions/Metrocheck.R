c927ca69b1ed4f3dd67742c73943b9bc31d6771c
613Martin <47364886+613Martin@users.noreply.github.com>
2019-02-20 12:59:12 +0100

    Update Metrocheck.R

--- functions/Metrocheck.R
+++ functions/Metrocheck.R
@@ -10,24 +10,20 @@ Metrocheck <- function(df) {
     if (!("Sjukhuskod" %in% colnames(df)))
         stop ("df has to include the column Sjukhuskod")
     ## Assign metropolitan variable
-    df$metropolitan <- ifelse(df$Sjukhuskod == 10011 | # St Görans sjukhus
-                              df$Sjukhuskod == 10013 | # SöS
-                              df$Sjukhuskod == 11002 | # Karolinska sjukhuset
-                              df$Sjukhuskod == 11002 | # Huddinge sjukhus 
-                              df$Sjukhuskod == 11003 | # Nya Karolinska
-                              df$Sjukhuskod == 11010 | # DS 
-                              df$Sjukhuskod == 11011 | # Södertälje sjukhus
-                              df$Sjukhuskod == 11012 | # Norrtälje sjukhus
+    df$metropolitan <- ifelse(df$Sjukhuskod == 10011 |  # St Görans sjukhus
+                              df$Sjukhuskod == 10013 |  # SöS
+                              df$Sjukhuskod == 11002 |  # Karolinska sjukhuset
+                              df$Sjukhuskod == 11002 |  # Huddinge sjukhus 
+                              df$Sjukhuskod == 11003 |  # Nya Karolinska
+                              df$Sjukhuskod == 11010 |  # DS 
+                              df$Sjukhuskod == 11011 |  # Södertälje sjukhus
+                              df$Sjukhuskod == 11012 |  # Norrtälje sjukhus
                               
-                              df$Sjukhuskod == 51001 | # Sahlgrenska
-                              df$Sjukhuskod == 51012 | # Kungälvs sjukhus
-                              df$Sjukhuskod == 51013 | # NU sjukvården
-                              df$Sjukhuskod == 51014 | # Södra Älvsborgs sjukhus
-                              df$Sjukhuskod == 52012 | # Allingsås lasarett
+                              df$Sjukhuskod == 51001 |  # Sahlgrenska
+                              df$Sjukhuskod == 51012 |  # Kungälvs sjukhus
                               
-                              df$Sjukhuskod == 41001 | # Skånes universitetssjukhus
-                              df$Sjukhuskod == 41011 | # Trelleborgs lasarett
-                              df$Sjukhuskod == 11012   # Helsinborgs lasarett
+                              df$Sjukhuskod == 41001 |  # Universitetssjukhuset Lund
+                              df$Sjukhuskod == 30001    # Universitetssjukhuset Malmö
                             , "Yes", "No")
     ## Return data.frame
     return(df)
