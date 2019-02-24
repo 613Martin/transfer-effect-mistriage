26b8343d190c00f7e4aed1c429a5f2bf55d5f442
613Martin <47364886+613Martin@users.noreply.github.com>
2019-02-24 20:59:43 +0100

    Create IndividualCentreCheck.R

---
+++ functions/IndividualCentreCheck.R
@@ -0,0 +1,24 @@
+#' IndividualCentreCheck
+#'
+#' Creates a table from a data.frame, based on number of deaths within 30 days of trauma.
+#' "Sjukhusnummer" with > 170 deaths within 30 days of trauma are considered valid individual centres.
+#' Adds a new colum called Valid_Individual_Centre
+#' If a "Sjukhusnummer" is a valid individual centre, adds Yes to the new colum, otherwise No.
+#' 
+#' @param df A dataframe. Must contain a column "Sjukhuskod". No default.
+IndividualCentreCheck <- function(df) {
+  
+    ## Error handling
+    if (!is.data.frame(df))
+        stop ("Input has to be a data.frame")
+    if (!("Sjukhuskod" %in% colnames(df)))
+        stop ("df has to include the column Sjukhuskod")
+  
+    ## Evaluates if Sjukhuskod is valid individual centre
+    x <- as.data.frame(table(df$Sjukhuskod, df$res_survival, exclude = 2))
+    y <- as.vector(x$Var1[x$Freq >= 170])
+  
+    df$Valid_Individual_Centre <- ifelse(df$Sjukhuskod %in% y, "Yes", "No")
+  
+    return(df)
+}
