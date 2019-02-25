f2d3d707a474bc3e1622b22c4199e240936f615c
613Martin <47364886+613Martin@users.noreply.github.com>
2019-02-25 13:49:00 +0100

    Create ImportStudyData.R

---
+++ functions/ImportStudyData.R
@@ -0,0 +1,23 @@
+#' ImportStudyData 
+#' 
+#' Imports the study data from a csv file.
+#' Uses the fread function, so remember to initialize the Hmisc package before running!
+#' @param data.file.name Character vector of length 1. The name of the study
+#'     data file. Defaults to NULL.
+#' @param data.path Character vector of length 1. The path to the data
+#'     directory. Defaults to "./data/"
+ImportStudyData <- function(data.file.name = NULL, data.path = "./data/") {
+
+    ## Error handling
+    if (is.null(data.file.name)) 
+        stop("You have to supply a data file name") 
+    if (!is.character(data.file.name)) 
+        stop("The data file name has to be a character string")
+    if (length(data.file.name) > 1) 
+        stop("The data file name has to be a character vector of length 1")
+    ## Combine data_path and data_file_name into file_path
+    file.path <- paste0(data.path, data.file.name)
+    ## Import study data from a csv file
+    study.data <- fread(file = file.path)
+        return(study.data)
+}
