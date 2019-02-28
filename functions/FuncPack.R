1746edf0e21a6c309bf6211f8a97a930422394ad
613Martin <47364886+613Martin@users.noreply.github.com>
2019-02-28 15:48:34 +0100

    Create FuncPack.R

---
+++ functions/FuncPack.R
@@ -0,0 +1,13 @@
+#' Loads packages and functions
+#' 
+#' No parameters or variables needed to run.
+FuncPack <- function() {
+  
+    ## Load required packages
+    library(data.table)
+    library(mice)
+    library(Hmisc)
+  
+    ## Source functions
+    invisible(lapply(list.files("./Functions", full.names = TRUE, pattern = ".R$"), source))
+}
