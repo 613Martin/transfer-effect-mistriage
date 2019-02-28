b633f24344139c290954f039960bf4fd8d2ed96d
613Martin <47364886+613Martin@users.noreply.github.com>
2019-02-28 15:32:35 +0100

    Create InclusionSelection.R

---
+++ functions/InclusionSelection.R
@@ -0,0 +1,15 @@
+#' InclusionSelection
+#'
+#' Selectes only cases with age > 15, or cases where age is registerd as NA.
+#' @param df Dataframe. The study sample. Must contain pt_age_yrs No default.
+InclusionSelection <- function(df) {
+  
+    ## Error handling
+    if (!is.data.frame(df))
+        stop ("Input has to be a data.frame")
+    if (!("pt_age_yrs" %in% colnames(df)))
+        stop ("df has to include the column pt_age_yrs")  
+    ## Case selection
+    df <- df[ which(df$pt_age_yrs > 15 | is.na(df$pt_age_yrs)), ]
+    return(df)
+}
