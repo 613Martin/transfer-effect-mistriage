506719cd3efb71546b39f2b5c81f59b38407b804
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-03-04 10:49:07 +0100

    Remove more which in subsetting operations

--- MainCodeRun.R
+++ MainCodeRun.R
@@ -30,13 +30,13 @@ MainCodeRun <- function() {
     ## Mark entries as Metropolitan or Non-Metropolitan
     selected.data.metro.mark <- Metrocheck(selected.data)
     ## Create Metropolitan Sample and Non-Metropolitan Sample
-    Metropolitan.Sample <- selected.data.metro.mark[ which(selected.data.metro.mark$metropolitan == "Yes"), ]
-    Non.Metropolitan.Sample <- selected.data.metro.mark[ which(selected.data.metro.mark$metropolitan == "No"), ]
+    Metropolitan.Sample <- selected.data.metro.mark[selected.data.metro.mark$metropolitan == "Yes", ]
+    Non.Metropolitan.Sample <- selected.data.metro.mark[selected.data.metro.mark$metropolitan == "No", ]
     rm(selected.data.metro.mark)
     ## Mark entries as valid (>170 events) individual centres
     selected.data.ind.mark <- IndividualCentreCheck(selected.data)
     ## Create Multi Centre Sample (= A combination of all valid individual centres)
-    Multi.Centre.Sample <- selected.data.ind.mark[ which(selected.data.ind.mark$Valid_Individual_Centre == "Yes"), ]
+    Multi.Centre.Sample <- selected.data.ind.mark[selected.data.ind.mark$Valid_Individual_Centre == "Yes", ]
     ## Create Individual Centre Sample 1-n, where n is the total number of valid individual centres 
     #? I´m trying to create a function that outputs a data.frame named Individual.Centre.Sample.1, Individual.Centre.Sample.2... and so on.
     #? My inital thought was extracting each unique "Sjukhuskod" present in Multi.Centre.Sample as its own data.frame, and having R name 
