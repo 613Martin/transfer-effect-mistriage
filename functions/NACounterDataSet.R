2d0627e5705af9400b849eb0f280e7e4ec081a79
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-03-07 10:46:14 +0100

    Replace list input with dataframe and replace _ with .

--- functions/NACounterDataSet.R
+++ functions/NACounterDataSet.R
@@ -1,28 +1,26 @@
 #' NACounterDataSet
 #'
-#' Inputs a data set in list form
-#' Ex. data.sets$high.volume.vs.low.volume
-#' Counts the number of NA in the inputted data set and calculates the percentage.
-#' Outputs a data frame with the total number of NA and the percentage of NA in that data frame
-#' (Meant to run over each data set)
-#' @param list List. A data set, containing two or more samples. No default.
-NACounterDataSet <- function(list) {
+#' Counts the number of NA in the inputted data set and calculates the
+#' percentage.  Outputs a data frame with the total number of NA and the
+#' percentage of NA in that data frame (Meant to run over each data set)
+#' @param df A dataframe. No default.
+NACounterDataSet <- function(df) {
   
     ## Error handling
-    if (!is.list(list))
-        stop ("Input has to be a list")
+    if (!is.data.frame(df))
+        stop ("Input has to be a data.frame")
     ## Count number of NA in the data set
-    y <- sapply(list, function(x) sum(is.na(x)))
-    total_number_of_na <- sum(y) 
+    y <- sapply(df, function(x) sum(is.na(x)))
+    total.number.of.na <- sum(y) 
     ## Percentage of NA
-    total_data_points <- sapply(list, function(x) ncol(x) * nrow(x))
-    total_data_points <- sum(total_data_points)
-    percent_NA <- total_number_of_na / total_data_points
-    percent_NA <- percent_NA * 100
-    percent_NA <- round(percent_NA, 2)
+    total.data.points <- sapply(df, function(x) ncol(x) * nrow(x))
+    total.data.points <- sum(total.data.points)
+    percent.NA <- total.number.of.na / total.data.points
+    percent.NA <- percent.NA * 100
+    percent.NA <- round(percent.NA, 2)
     ## Combine to output
-    output <- as.data.frame(total_number_of_na)
-    output$percent_NA <- percent_NA
+    output <- as.data.frame(total.number.of.na)
+    output$percent.NA <- percent.NA
     ## Return new data.frame with NA data from data set
     return(output)
 }
