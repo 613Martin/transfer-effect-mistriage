e6ec04ccf8a65df091139c0d481ddf939c850b80
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-03-07 10:53:13 +0100

    Rewrite to calculate incomplete observations

--- functions/NACounterDataSet.R
+++ functions/NACounterDataSet.R
@@ -9,18 +9,11 @@ NACounterDataSet <- function(df) {
     ## Error handling
     if (!is.data.frame(df))
         stop ("Input has to be a data.frame")
-    ## Count number of NA in the data set
-    y <- sapply(df, function(x) sum(is.na(x)))
-    total.number.of.na <- sum(y) 
-    ## Percentage of NA
-    total.data.points <- sapply(df, function(x) ncol(x) * nrow(x))
-    total.data.points <- sum(total.data.points)
-    percent.NA <- total.number.of.na / total.data.points
-    percent.NA <- percent.NA * 100
-    percent.NA <- round(percent.NA, 2)
+    ## Calculate number of observations with missing values
+    total.number.of.na <- nrow(df) - nrow(df[complete.cases(df), ])
     ## Combine to output
     output <- as.data.frame(total.number.of.na)
-    output$percent.NA <- percent.NA
+    output$percent.NA <- round(total.number.of.na/nrow(df) * 100)
     ## Return new data.frame with NA data from data set
     return(output)
 }
