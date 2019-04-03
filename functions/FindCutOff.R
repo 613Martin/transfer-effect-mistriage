47767d0592ee29765a2ce1318b9da9085958a091
613Martin <47364886+613Martin@users.noreply.github.com>
2019-04-03 13:09:20 +0200

    Update FindCutOff.R

--- functions/FindCutOff.R
+++ functions/FindCutOff.R
@@ -1,63 +1,47 @@
 #' FindCutOff
 #' 
-#' Inputs a data.frame. Use Grid data frame.
+#' Inputs a list with probabilities, and a grid against which to test them.
 #' Finds the cut off value which provides the best triage rate.
-#' @param df Dataframe. 
-FindCutOff <- function(df) {
+#' @param prob.list. A list of all unique probabilities to be tested.
+#' @param grid. A data frame. Data frame with probabilities and outcomes for all entries.
+FindCutOff <- function(prob.list, grid) {
   
-  ## Error handling
-  if (!is.data.frame(df))
-   stop ("Input has to be a data.frame")
+  ## Create data frame which to save triage results
+  probs.triage.value <- as.data.frame(unlist(prob.list))
+  names(probs.triage.value) <- c("probs")
   
-  ## Setup valid cutoff vector
-  valid.cutoffs <- vector()
-  ## Setup best cutoff vector, and set to 1
-  best.cutoff <- vector()
-  best.cutoff <- 1
-  
-  ## Finding all valid cutoffs (undertriage rate <0.05)
-  ## Set cutoff to test
-  for (i in unique(df$prob)) {
-   tested.cut.off <- i
-   ## Set tested data (all major traumas according to cutoff)
-   tested.data.major <- df[df$prob >= tested.cut.off,]
-   ## Set tested data (all minor traumas according to cutoff)
-   tested.data.minor <- df[df$prob < tested.cut.off,]
-   ## Estimate undertriage in tested data (ISS_over_15 = 1 = Yes)
-   num.of.undertriage <- sum(tested.data.minor$ISS_over_15 == 1)      
-   undertriage.rate <- num.of.undertriage / nrow(df)
-   ## Estimate overtriage in tested data
-   num.of.overtriage <- sum(tested.data.major$ISS_over_15 == 2)
-   overtriage.rate <- num.of.overtriage / nrow(df)
-   ## Estimate mistriage rate
-   mistriage.rate <- undertriage.rate + overtriage.rate
-   ## Evaluate undertriage and if valid add to valid.cutoffs
-   if (undertriage.rate <= 0.05) {
-    valid.cutoffs <- c(valid.cutoffs, tested.cut.off)
-   }
-  }
-  
-  ## Finding best cutoff (optimal overtriage)
-  ## Test all valid cutoffs for the one that is best in terms of overtriage
-  for (x in unique(valid.cutoffs)) {
-    tested.valid.cut.off <- x
+  ## Calculate undertriage for each probability
+  probs.triage.value$undertriage <- unlist(lapply(prob.list, function(x) {
     ## Set tested data (all major traumas according to cutoff)
-    tested.data.major <- df[df$prob >= tested.valid.cut.off,]
+    tested.data.major <- grid[grid$probs >= x,]
     ## Set tested data (all minor traumas according to cutoff)
-    tested.data.minor <- df[df$prob < tested.valid.cut.off,]
-    ## Estimate undertriage in tested data (ISS_over_15 = 1 = Yes)
+    tested.data.minor <- grid[grid$probs < x,]
+    ## Calculate undertriage in tested data (ISS_over_15 = 1 = Yes)
     num.of.undertriage <- sum(tested.data.minor$ISS_over_15 == 1)      
-    undertriage.rate <- num.of.undertriage / nrow(df)
-    ## Estimate overtriage in tested data
+    undertriage.rate <- num.of.undertriage / nrow(grid)
+    ## Return to reults data frame
+    return(undertriage.rate)     
+    }
+    ))
+  
+  ## Calculate overtriage for each probability
+  probs.triage.value$overtriage <- unlist(lapply(prob.list, function(x) {
+    ## Set tested data (all major traumas according to cutoff)
+    tested.data.major <- grid[grid$probs >= x,]
+    ## Set tested data (all minor traumas according to cutoff)
+    tested.data.minor <- grid[grid$probs < x,]
+    ## Calculate overtriage in tested data
     num.of.overtriage <- sum(tested.data.major$ISS_over_15 == 2)
     overtriage.rate <- num.of.overtriage / nrow(df)
-    ## Estimate mistriage rate
-    mistriage.rate <- undertriage.rate + overtriage.rate
-    ## Evaluate overtriage and select best cutoff
-    if (overtriage.rate <= best.overtriage.rate) {
-      best.cutoff <- tested.valid.cut.off
-    } 
-  }
-  ## Return the best cutoff!
-  return(best.cutoff)
-  }
+    ## Return to reults data frame
+    return(overtriage.rate)     
+    }
+    ))
+  
+ ## Find best cutoff
+ valid.cutoffs <- probs.triage.value[probs.triage.value$undertriage <= 0.5,]
+ best.cutoff <- valid.cutoffs[min(valid.cutoffs$overtriage),]
+ ## Return optimal cutoff    
+ return(best.cutoff$probs)
+
+}
