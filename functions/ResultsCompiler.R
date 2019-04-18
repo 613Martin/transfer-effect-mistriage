7a95e1f5df33b81ad240ce5534fe601ae798037b
613Martin <47364886+613Martin@users.noreply.github.com>
2019-04-18 11:36:06 +0200

    Update ResultsCompiler.R

--- functions/ResultsCompiler.R
+++ functions/ResultsCompiler.R
@@ -1,64 +1,33 @@
 #' ResultsCompiler
 #'
-#' Uses data.sets with local mistriage data combines this with comparison data to create
-#' a results data frame.
-#' Vectors used: Data set, sample, imputation, local mistriage, transfer mistriage, difference.
-#'
-#' @param data.sets A list.
-#' @param comparison.data. A list.
-ResultsCompiler <- function(list.of.data.sets, comparison.data){
+#' Takes the list of values and calculates and prepares results,
+#' @param combined.split.data.sets List with combination of required values.
+ResultsCompiler <- function(combined.split.data.sets){
   
-  ## Error handling
-
-  ## Single out local mistriage values with corresponding imputation
-  local.mistriage <- lapply(list.of.data.sets, function(sample) lapply(sample, function(imp) (lapply(imp, function(x) {
-    imputation <- median(x[[2]][[1]])
-    local.mistriage <- x[[7]]
-    x <- data.frame(imputation, local.mistriage)
-    })))) 
-  ## Combine all imputations to one data frame in each sample
-  combined.by.imp <- lapply(local.mistriage, function(sample) lapply(sample, function(df) {
+  ## Convert all imutations to data frames
+  combined.split.data.sets.data.frames <- lapply(combined.split.data.sets, function(sample) lapply(sample, function(imp) (lapply(imp, function(x) {
+    x <- data.frame(x)
+  }))))
+  ## Combine all data frames in a sample using rbind
+  sample.data.sets <- lapply(combined.split.data.sets.data.frames, function(sample) lapply(sample, function(df) {
     df1 <- df[[1]]
     for (x in 2:length(df)) {
-      df1 <- merge(df1, df[[x]], all = TRUE)
-    }
-      return(df1)
-      }))
-  ##  Single out transfer mistriage value
-  transfer.mistriage <- lapply(comparison.data, function(sample) lapply(sample, function(imp) (lapply(imp, function(x) {
-    transfer.mistriage <- x[[1]]
-    x <- data.frame(transfer.mistriage)
-  })))) 
-  ## Combine to one data frame in each sample
-  combined.transfer <- lapply(transfer.mistriage, function(sample) lapply(sample, function(df) {
-    df1 <- df[[1]]
-    for (x in 2:length(df)) {
-      df1 <- merge(df1, df[[x]], all = TRUE, sort = FALSE)
+      df1 <- rbind(df1, df[[x]])
     }
     return(df1)
   }))
-  ## Combine local mistriage and transfer mistriage into one dataframe per sample
-  for (i in 1:length(combined.by.imp)) {
-    for (u in 1:length(combined.by.imp[[i]])) {
-      combined.by.imp[[i]][[u]] <- cbind(combined.by.imp[[i]][[u]], combined.transfer[[i]][[u]])
-    }
-  }
-  
-  ## Calculate difference, local performance vs transferred performance of local model
-  results.with.diff <- lapply(combined.by.imp, function(sample) lapply(sample, function(x) {
-    x$transfer.performance.minus.local.performance <- x[[3]] - x[[2]]
-    return(x)
-    }))
   ## Calculate difference, local performance vs transferred model, transferred model performance minus local model performance
-  all.results.with.diff <- lapply(results.with.diff, function(x) {
-    x[[1]]$transferred.performance.minus.buddy.local.performance <- x[[1]][[3]] - x[[2]][[2]]
-    x[[2]]$transferred.performance.minus.buddy.local.performance <- x[[2]][[3]] - x[[1]][[2]]
+  sample.data.sets.with.comparison <- lapply(sample.data.sets, function(x) {
+    ## Top sample (i.e. High volume, or metropolitan)
+    x[[1]]$Transferred.mistriage.minus.buddy.local.mistriage <- x[[1]][[5]] - x[[2]][[2]]
+    x[[1]]$Transferred.undertriage.minus.buddy.local.undertriage <- x[[1]][[6]] - x[[2]][[3]]
+    x[[1]]$Transferred.overtriage.minus.buddy.local.overtriage <- x[[1]][[7]] - x[[2]][[4]]
+    ## Bottom sample (i.e. Low volume, or non-metropolitan)
+    x[[2]]$Transferred.mistriage.minus.buddy.local.mistriage <- x[[2]][[5]] - x[[1]][[2]]
+    x[[2]]$Transferred.undertriage.minus.buddy.local.undertriage <- x[[2]][[6]] - x[[1]][[3]]
+    x[[2]]$Transferred.overtriage.minus.buddy.local.overtriage <- x[[2]][[7]] - x[[1]][[4]]
     return(x)
   })
-  
-  ## Create output
-  output <- all.results.with.diff
   ## Return output
-  return(output)
+  return(sample.data.sets.with.comparison)
 }
-
