48341042cd18458a61d3edbe706b3e1bb4d61ce3
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-04-22 08:11:52 +0200

    Rename objects and use name indexing

--- functions/FindOptimalCutOff.R
+++ functions/FindOptimalCutOff.R
@@ -3,21 +3,18 @@
 #' Finds the optimal cutoff for the development model.
 #' Outputs this value and adds it to the list for each respective sample(=model)
 #' Dependant on the function: FindCutOff
-#' Reference:
-#´ df[[1]] = Development data in each sample
-#´ df[[2]] = Validation data in each sample
-#´ df[[3]] = Coeffs
-#´ df[[4]] = Dev grid
-#´ df[[5]] = Val grid
-#' @param df Dataframe.
-FindOptimalCutOff <- function(df) {
+#' @param df.list List of dataframes.
+FindOptimalCutOff <- function(df.list) {
 
-  ## Create list of probabilities
-  list.of.probs <- as.list(unique(df[[4]]$probs))
-  ## Search for, and select the optimal cutoff
-  optimal.cutoff <- FindCutOff(prob.list = list.of.probs, grid = df[[4]])
-  ## Create output
-  output <- list("Development" = df[[1]], "Validation" = df[[2]],  "Model coefficients" = df[[3]], "Development grid" = df[[4]], "Validation grid" = df[[5]], "Optimal CutOff" = optimal.cutoff)
-  ## Return output
-  return(output)
+    ## Get grid
+    grid <- df.list[["Development grid"]]
+    ## Create list of probabilities
+    prob.list <- as.list(unique(grid$probs))
+    ## Search for, and select the optimal cutoff
+    optimal.cutoff <- FindCutOff(prob.list = prob.list, grid = grid)
+    ## Create output
+    output <- df.list
+    output[["Optimal CutOff"]] = optimal.cutoff
+    ## Return output
+    return(output)
 }
