68c5d2c3bc88858f677eb947c0f031a7a92d15d5
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-04-24 00:24:32 +0200

    Add argument to return packages and functions instead of loading

--- functions/FuncPack.R
+++ functions/FuncPack.R
@@ -1,17 +1,31 @@
-#' Loads packages and functions
+#' Loads or return packages and functions
 #' 
-#' No parameters or variables needed to run.
-FuncPack <- function() {
-  
+#' @param return.only Logical. If TRUE packages and functions are not loaded,
+#'     but returned. Defaults to FALSE.
+#' @export
+FuncPack <- function(return.only = FALSE) {
+    
     ## Load required packages
-    library(data.table)
-    library(mice)
-    library(Hmisc)
-    library(boot)
-    library(bengaltiger)
-    library(doParallel)
-    library(foreach)
-  
+    packages <- c("data.table",
+                  "mice",
+                  "Hmisc",
+                  "boot",
+                  "bengaltiger",
+                  "doParallel",
+                  "foreach")
+
+    if (!return.only)
+        for(package in packages) library(package, character.only = TRUE)
+    
     ## Source functions
-    invisible(lapply(list.files("./Functions", full.names = TRUE, pattern = ".R$"), source))
+    files <- list.files("./Functions", full.names = TRUE, pattern = ".R$")
+    functions <- gsub("./Functions/|.R", "", files)
+    if (!return.only)
+        invisible(lapply(files, source))
+
+    ## Return packages and functions
+    return.object <- list(packages = packages,
+                          functions = functions)
+    if (return.only)
+        return(return.object)
 }
