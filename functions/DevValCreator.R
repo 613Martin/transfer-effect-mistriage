fac3d8d2599cd889f6e42b8592048db75142b86c
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-08-17 12:31:03 +0200

    Add tryCatch

--- functions/DevValCreator.R
+++ functions/DevValCreator.R
@@ -14,8 +14,14 @@ DevValCreator <- function(df) {
     ## Selecting events only
     events.only <- df[which(df$res_survival == "Dead"),]
     
-    ## Sorting by date (new line 190815)
-    sorted.events.only <- events.only[order(as.Date(as.character(events.only$DateTime_Of_Trauma, "%Y%m%d")), decreasing = FALSE),]
+    ## Sorting by date (new segment 20190817, tryCatch added)
+    sorted.events.only <- tryCatch(expr = events.only[order(as.Date(as.character(events.only$DateTime_Of_Trauma, "%Y%m%d")), decreasing = FALSE),],
+                                   error = function(e) {
+                                       .imp <- unique(df$.imp)
+                                       filename <- paste0("output/failed.imputation.", .imp, ".", gsub(":|-| ", "", format(Sys.time())), ".Rds")
+                                       saveRDS(df, filename)
+                                       stop(paste0("Error on .imp ", .imp, ", saved as ", filename, ": ", e))
+                                   })
     ## Sorting by date (old line)
     # sorted.events.only <- events.only[order(as.Date(events.only$DateTime_Of_Trauma), decreasing = FALSE),]
     
