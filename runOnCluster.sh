c02604312f8f44565be0982b68eb0a5d72f64c86
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-09-02 20:47:13 +0200

    Update to run on cluster

--- runOnCluster.sh
+++ runOnCluster.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#SBATCH -t 02:00:00
-#SBATCH -N 1
+#SBATCH -t 48:00:00
+#SBATCH -N 20
 #SBATCH --tasks-per-node=20
 #SBATCH --exclusive
 #SBATCH --mail-user=martingerdin@gmail.com
 #SBATCH --mail-type=END
 
-mpirun R --slave -f MainCodeRun.R
+module load GCC/7.3.0-2.30 OpenMPI/3.1.1 R/3.5.1
 
+mpirun --bind-to core R --slave -f MainCodeRun.R
