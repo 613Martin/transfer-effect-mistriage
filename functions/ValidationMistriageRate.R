21e5b527bca230e16c49a21eed62858a1c9dc291
613Martin <47364886+613Martin@users.noreply.github.com>
2020-06-07 21:22:41 +0200

    Update ValidationMistriageRate.R

--- functions/ValidationMistriageRate.R
+++ functions/ValidationMistriageRate.R
@@ -1,6 +1,7 @@
 #' ValidationMistriageRate
 #' 
 #' Finds mistriage rate in each sample using the "local" model and cutoff.
+#' Also calculates traditional epidemiological values and presents these.
 #' Outputs this value and adds it to the original list
 #' Dependant on the function: CalculateMistriage.R
 
@@ -8,15 +9,27 @@
 ValidationMistriageRate <- function(df.list) {
     
     ## Find mistriage in each sample
-    undertriage.overtriage <- CalculateUndertriageOvertriage(data = df.list$Validation, model = df.list[["Model coefficients"]], cutoff = df.list[["Optimal CutOff"]])
-    undertriage <- undertriage.overtriage["undertriage"]
-    overtriage <- undertriage.overtriage["overtriage"]
+    undertriage.overtriage.traditional <- CalculateUndertriageOvertriage(data = df.list$Validation, model = df.list[["Model coefficients"]], cutoff = df.list[["Optimal CutOff"]])
+    undertriage <- undertriage.overtriage.traditional["undertriage"]
+    overtriage <- undertriage.overtriage.traditional["overtriage"]
     mistriage.rate <- undertriage + overtriage
+    
     ## Create output
     output <- df.list
     output[["Validation mistriage"]] <- mistriage.rate
     output[["Validation undertriage"]] <- undertriage
     output[["Validation overtriage"]] <- overtriage
+    
+    ## Add traditional values to output
+    sensitivity <- undertriage.overtriage.traditional["sensitivity"]
+    specificity <- undertriage.overtriage.traditional["specificity"]
+    PPV <- undertriage.overtriage.traditional["PPV"]
+    NPV <- undertriage.overtriage.traditional["NPV"]
+    output[["Validation sensitivity"]] <- sensitivity
+    output[["Validation specificity"]] <- specificity
+    output[["Validation PPV"]] <- PPV
+    output[["Validation NPV"]] <- NPV
+    
     ## Return output
     return(output)
 
