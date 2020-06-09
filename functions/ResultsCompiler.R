46cf6eddce84162bc3f5e06f2a979531667860af
613Martin <47364886+613Martin@users.noreply.github.com>
2020-06-09 21:09:47 +0200

    Update ResultsCompiler.R

--- functions/ResultsCompiler.R
+++ functions/ResultsCompiler.R
@@ -20,12 +20,51 @@ ResultsCompiler <- function(combined.split.datasets){
         x[[1]]$Transferred.mistriage.minus.buddy.local.mistriage <- x[[1]][["Transfer.mistriage"]] - x[[2]][["Validation.mistriage"]]
         x[[1]]$Transferred.undertriage.minus.buddy.local.undertriage <- x[[1]][["Transfer.undertriage"]] - x[[2]][["Validation.undertriage"]]
         x[[1]]$Transferred.overtriage.minus.buddy.local.overtriage <- x[[1]][["Transfer.overtriage"]] - x[[2]][["Validation.overtriage"]]
+        x[[1]]$Transferred.sensitivity.minus.buddy.local.sensitivity <- x[[1]][["Transfer.sensitivity"]] - x[[2]][["Validation.sensitivity"]]
+        x[[1]]$Transferred.specificity.minus.buddy.local.specificity <- x[[1]][["Transfer.specificity"]] - x[[2]][["Validation.specificity"]]
+        x[[1]]$Transferred.PPV.minus.buddy.local.PPV <- x[[1]][["Transfer.PPV"]] - x[[2]][["Validation.PPV"]]
+        x[[1]]$Transferred.NPV.minus.buddy.local.NPV <- x[[1]][["Transfer.NPV"]] - x[[2]][["Validation.NPV"]]
+        
         ## Bottom sample (i.e. Low volume, or non-metropolitan)
         x[[2]]$Transferred.mistriage.minus.buddy.local.mistriage <- x[[2]][["Transfer.mistriage"]] - x[[1]][["Validation.mistriage"]]
         x[[2]]$Transferred.undertriage.minus.buddy.local.undertriage <- x[[2]][["Transfer.undertriage"]] - x[[1]][["Validation.undertriage"]]
         x[[2]]$Transferred.overtriage.minus.buddy.local.overtriage <- x[[2]][["Transfer.overtriage"]] - x[[1]][["Validation.overtriage"]]
+        x[[2]]$Transferred.sensitivity.minus.buddy.local.sensitivity <- x[[2]][["Transfer.sensitivity"]] - x[[1]][["Validation.sensitivity"]]
+        x[[2]]$Transferred.specificity.minus.buddy.local.specificity <- x[[2]][["Transfer.specificity"]] - x[[1]][["Validation.specificity"]]
+        x[[2]]$Transferred.PPV.minus.buddy.local.PPV <- x[[2]][["Transfer.PPV"]] - x[[1]][["Validation.PPV"]]
+        x[[2]]$Transferred.NPV.minus.buddy.local.NPV <- x[[2]][["Transfer.NPV"]] - x[[1]][["Validation.NPV"]]
         return(x)
     })
+    
+    ## Rearrange colums in each imputation data frame
+    col_order <- c("Imputation", 
+                   "Validation.mistriage", 
+                   "Validation.undertriage", 
+                   "Validation.overtriage", 
+                   "Transfer.mistriage", 
+                   "Transfer.undertriage", 
+                   "Transfer.overtriage", 
+                   "Transferred.mistriage.minus.buddy.local.mistriage",
+                   "Transferred.undertriage.minus.buddy.local.undertriage",
+                   "Transferred.overtriage.minus.buddy.local.overtriage",
+                   "Validation.sensitivity",
+                   "Validation.specificity",
+                   "Validation.PPV",
+                   "Validation.NPV",
+                   "Transfer.sensitivity",
+                   "Transfer.specificity",
+                   "Transfer.PPV",
+                   "Transfer.NPV",
+                   "Transferred.sensitivity.minus.buddy.local.sensitivity",
+                   "Transferred.specificity.minus.buddy.local.specificity",
+                   "Transferred.PPV.minus.buddy.local.PPV",
+                   "Transferred.NPV.minus.buddy.local.NPV"
+                   )
+    sample.datasets.with.comparison <- lapply(sample.datasets.with.comparison, function(sample) lapply(sample, function(df) {
+      df <- df[, col_order]
+      return(df)
+    }))
+    
     ## Return output
     return(sample.datasets.with.comparison)
 
